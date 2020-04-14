cfda7e5e64338796f420295e0281b830c8b538ed
Hamid Dezhkam <hamiddezhkam@thegoodguys.com.au>
2020-04-15 08:22:30 +1000

    rev 15012020

---
+++ burger-app/src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import classes from './BuildControls.module.css'
+import BuildControl from './BuildControl/BuildControl';
+
+
+const controls = [
+    { lable: 'Salad', type: 'salad' },
+    { lable: 'Bacon', type: 'bacon' },
+    { lable: 'Cheese', type: 'cheese' },
+    { lable: 'Meat', type: 'meat' }
+]
+
+const BuildControls = (props) => {
+    return (
+        <div className={classes.BuildControls}>
+            <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+            {controls.map(ctrl => (
+                <BuildControl
+                    lable={ctrl.lable}
+                    type={ctrl.type}
+                    key={ctrl.lable}
+                    added={() => props.ingredientAdded(ctrl.type)}
+                    removed={()=> props.ingredientRemoved(ctrl.type)}
+                    disabled={props.disabled[ctrl.type]}
+                />
+            ))}
+            <button 
+            className={classes.OrderButton}
+            disabled ={!props.purchaseable}
+            >ORDER NOW
+            </button>
+        </div>
+    )
+}
+
+export default BuildControls
