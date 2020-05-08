4b0ad71028f141412cfd6c25e04e8c18f75b1fbc
Hamid Dezhkam <hamiddezhkam@thegoodguys.com.au>
2020-05-08 13:40:11 +1000

    Redux completed

--- burger-app/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-app/src/hoc/withErrorHandler/withErrorHandler.js
@@ -11,28 +11,23 @@ const withErrorHandler = (WrappedComponent, axios) => {
             error: null
         }
 
-        componentDidMount() {
-
-            this.reqInterceptor = axios.interceptors.request.use(req => {
-                this.setState({ error: null })
-                return req
-            })
-
-            this.resInterceptor = axios.interceptors.request.use(res => {
-                axios.interceptors.response.use(res => res, error => {
-                    this.setState({ error: error })
-                })
-            })
+        componentWillMount () {
+            this.reqInterceptor = axios.interceptors.request.use( req => {
+                this.setState( { error: null } );
+                return req;
+            } );
+            this.resInterceptor = axios.interceptors.response.use( res => res, error => {
+                this.setState( { error: error } );
+            } );
         }
 
-        //  componentWillUnmount() {
-        //      axios.interceptors.request.ejecet(this.reqInterceptor)
-        //      axios.interceptors.response.ejecet(this.resInterceptor)
-        //  }
+          componentWillUnmount() {
+              axios.interceptors.request.eject(this.reqInterceptor)
+              axios.interceptors.response.eject(this.resInterceptor)
+          }
 
         errorConfirmedHandler = () => {
             this.setState({ error: null })
-            console.log('j');
         }
 
         render() {
@@ -40,17 +35,15 @@ const withErrorHandler = (WrappedComponent, axios) => {
                 <Aux>
                     <Modal
                         show={this.state.error}
-                        modalClosed={this.errorConfirmedHandler}
-                    >
+                        modalClosed={this.errorConfirmedHandler}>
                         {this.state.error ? this.state.error.message : null}
                     </Modal>
                     <WrappedComponent {...this.props} />
                 </Aux>
-            )
-
+            );
         }
     }
 }
 
 
-export default withErrorHandler
+export default withErrorHandler;
--- burger-app/src/index.js
+++ burger-app/src/index.js
@@ -4,14 +4,31 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+//import thunk for apply asyncronize midleware function
+import thunk from 'redux-thunk'
+import burgerBuilderReducer from './store/reducers/burgerBuilder'
+import orderReducer from './store/reducers/order'
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+    burgerBuilder: burgerBuilderReducer,
+    order: orderReducer
+})
+
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk)
+))
 
 const app = (
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
 )
 
 
