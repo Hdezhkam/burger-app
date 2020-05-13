3da6f80fc41a176507b70d29c9f20d145e1501a9
Hamid Dezhkam <hamiddezhkam@thegoodguys.com.au>
2020-05-13 11:02:08 +1000

    Auth part

--- burger-app/src/App.js
+++ burger-app/src/App.js
@@ -4,6 +4,7 @@ import Layout from './hoc/Layout/Layout';
 import Checkout from './containers/Checkout/Checkout';
 import { Route, Switch } from 'react-router-dom'
 import Orders from './containers/Checkout/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 
 
 
@@ -16,6 +17,7 @@ class App extends Component {
         <Switch>
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={Auth}/>
           <Route path="/" exact component={BurgerBuilder} />
         </Switch>
       </Layout>
